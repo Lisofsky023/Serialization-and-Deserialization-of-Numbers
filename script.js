a0fbd54d38fcea322ee1eaeb7ab79b6969a4c02a
Lisofsky023 <lisa.2023.ge@gmail.com>
2023-11-13 20:16:20 +0400

    finish

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// Функция для генерации случайных чисел
 const generateRandomNumbers = (count, min, max) =>
   Array.from({ length: count }, () => Math.floor(Math.random() * (max - min + 1)) + min);
 
-// Функция для генерации тестовых случаев
 const generateTestCases = () => ({
     random_50: generateRandomNumbers(50, 1, 300),
     random_100: generateRandomNumbers(100, 1, 300),
@@ -14,55 +12,42 @@ const generateTestCases = () => ({
     three_of_each: Array.from({ length: 300 }, (_, i) => i + 1).flatMap(num => [num, num, num])
 });
 
-// Функция сериализации
-const serialize = (numbers) => {
-    numbers.sort((a, b) => a - b);
+const serializeImproved = (numbers) => {
+  numbers.sort((a, b) => a - b);
 
-    return numbers.reduce((result, num, i) => {
-        if (i === 0 || num !== numbers[i - 1] && num !== numbers[i - 1] + 1) {
-            result.push(num);
-        } else if (num === numbers[i - 1]) {
-            const last = result[result.length - 1];
-            if (typeof last === 'string' && last.includes('*')) {
-                result[result.length - 1] = `${num}*${parseInt(last.split('*')[1], 10) + 1}`;
-            } else {
-                result[result.length - 1] = `${num}*2`;
-            }
-        } else {
-            const last = result[result.length - 1];
-            if (typeof last === 'string' && last.includes('-')) {
-                result[result.length - 1] = `${last.split('-')[0]}-${num}`;
-            } else {
-                result[result.length - 1] = `${last}-${num}`;
-            }
-        }
-        return result;
-    }, []).join(",");
+  let bitmask = new Array(300).fill(0);
+  numbers.forEach(num => bitmask[num - 1] = 1);
+
+  let serialized = '';
+  let last = 0;
+
+  for (let i = 0; i < bitmask.length; i++) {
+    if (bitmask[i] === 1) {
+      let delta = i - last;
+      serialized += delta.toString(36) + ',';
+      last = i;
+    }
+  }
+
+  return serialized.slice(0, -1);
 };
 
-// Функция десериализации
-const deserialize = (serialized) =>
-  serialized.split(",").flatMap(part => {
-      if (part.includes('-')) {
-          let [start, end] = part.split('-').map(Number);
-          return Array.from({ length: end - start + 1 }, (_, i) => start + i);
-      } else if (part.includes('*')) {
-          let [num, count] = part.split('*').map(Number);
-          return Array.from({ length: count }, () => num);
-      }
-      return Number(part);
-  });
+const deserializeImproved = (serialized) => {
+  let deltas = serialized.split(',');
+  let numbers = [];
+  let last = 0;
 
-const testCases = generateTestCases();
-console.log(testCases);
+  for (let delta of deltas) {
+    last += parseInt(delta, 36);
+    numbers.push(last + 1);
+  }
 
-let numbers = [1, 2, 2, 2, 3, 4, 5, 6, 7];
-let serialized = serialize(numbers);
-let deserialized = deserialize(serialized);
+  return numbers;
+};
 
-console.log("Исходный массив:", numbers);
-console.log("Сериализованная строка:", serialized);
-console.log("Десериализованный массив:", deserialized);
+const calculateCompressionRate = (original, compressed) => {
+    return ((original.length - compressed.length) / original.length) * 100;
+};
 
 document.getElementById('generate').addEventListener('click', () => {
   const testCases = generateTestCases();
@@ -71,12 +56,15 @@ document.getElementById('generate').addEventListener('click', () => {
 
 document.getElementById('serialize').addEventListener('click', () => {
   const testData = JSON.parse(document.getElementById('test-data').textContent);
-  const serialized = serialize(testData.random_50);
+  const serialized = serializeImproved(testData.random_50);
   document.getElementById('serialized-data').textContent = serialized;
+
+  const compressionRate = calculateCompressionRate(JSON.stringify(testData.random_50), serialized);
+  document.getElementById('compression-rate').textContent = `Compression Rate: ${compressionRate.toFixed(2)}%`;
 });
 
 document.getElementById('deserialize').addEventListener('click', () => {
   const serializedData = document.getElementById('serialized-data').textContent;
-  const deserialized = deserialize(serializedData);
+  const deserialized = deserializeImproved(serializedData);
   document.getElementById('deserialized-data').textContent = JSON.stringify(deserialized, null, 2);
 });
